f8a412218bf5f33a08f2b6fcbbd26d17fc9c2892
girirajbidwai <bidwaigiriraj7@gmail.com>
2025-01-03 08:56:50 +0530

    Added new features or files

---
+++ src/main/java/com/giri/SpringSec/SpringSecApplication.java
@@ -0,0 +1,13 @@
+package com.giri.SpringSec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecApplication.class, args);
+	}
+
+}
